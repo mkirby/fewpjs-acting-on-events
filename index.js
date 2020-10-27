c45042ff2a2c99e44bc27c221bb1cdb6bdf2862c
Matthew Kirby <matt.kirby@yahoo.com>
2020-10-27 15:17:06 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+
+dodger.style.backgroundColor = "red";
+dodger.style.bottom = "5px";
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+        moveDodgerRight()
+    }
+});
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px");
+    let left = parseInt(leftNumbers);
+    
+    if (left > 0) {
+        dodger.style.left = `${left - 15}px`;
+    }
+}
+
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px");
+    let left = parseInt(leftNumbers);
+    
+    if (left < 360) {
+        dodger.style.left = `${left + 15}px`;
+    }
+}
